f0504568bacd443ad2b2deb92aa7f930e9f9f2e0
Harshvardhan0009 <148997077+Harshvardhan0009@users.noreply.github.com>
2023-11-29 20:05:52 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,116 @@
+# def greet(*name):
+#     print("the youngest child is"+name[0])
+# greet("Emil","tobias")
+# def r(*args):
+#     return min(args),max(args)
+# print(r(1,2,3,4,5,6,7,8,9))
+# fun=lambda x,z:x+z
+# w=int(input("w: "))
+# a=int(input("a: "))
+# print(fun(w,a))
+# lis=[2,3,4,5,6,7,8,]
+# lis1=[0,9,8,7,6,5,4,3,2,1]
+# def Sven(x):
+#     if x%2==0:
+#         return x
+# print(list(map(Sven,lis)))
+# def sums(x,y):
+#     return x+y
+# print(list(map(sums,lis,lis1)))
+# print(list(filter(lambda x:x in lis,lis1)))
+# print(list(filter(lambda x:x%2==0 in lis,lis1)))
+# global message
+# message=1234
+# def greet():
+#     messa="harsh"
+#     print(message)
+#     print(messa)
+# greet()
+# print(message)
+# def add(x):
+#     return x+2
+# def mult(x):
+#     return x*2
+# print(mult(add(7)))
+# x="string is how are you"
+# print(x[-7:-4:])
+# print(x[-9:-6:])
+# print(x[-3: ])
+# print(x[-1:-4:-1])
+# x=input("enter: ")
+# def mypro(r):
+#     if len(r)>3:
+#         d=r[0]
+#         e=r[-2: ]
+#         res=d+e
+#         return print(res)
+#     else:
+#         return print(r)
+# mypro(x)
+# str1=input("string")
+# n=int(input("no"))
+# print(n*str1)
+# print(k.capitalize())
+# print(k.upper())
+# o="HaRSH"
+# print(o.lower())
+# print(k.title())
+# print(k.swapcase())
+# integ="1,2,3,4,5,6"
+# list1=integ.split(",")
+# print(list1)
+# print(integ.center(22,"*"))
+# x="Tushar is cute boy boy"
+# print((x.count("boy")))
+# xp="java is easy"
+# xo=xp.replace("java","C++")
+# print(xo)
+# t="harsh"
+# l1=["dncdjns","basba","ddhdbb"]
+# x=t.join(l1)
+# print(type(x))
+# print(t.join(l1))
+ 
+# str1=input("enter")
+# str2=input("enter")
+# def rem(a,b):
+#     if len(a)==1 and len(b)==1:
+#         print("null")
+#     else:
+#         print(a[1: ]+b[1: ])
+# rem(str1,str2)
+# h='bpythonz'
+# print(min(h),max(h))
+# a=[1,2,3,4,5,6,7]
+# b=[1,2,3,4,5,6,7]
+# print(a is b)
+# print(b is a)
+# b=a
+# print(a is b)
+# a=[1,2,3,4,5,6,7]
+# b=list(a)
+# print(b)
+# a[0]=100
+# print(a)
+# print(b)
+# a=[1,2,3,4,5,6,7]
+# print(a)
+# b=a.pop(-1)
+# print(b)
+# print(a)
+# b=a.pop(-1)
+# print(b)
+# print(a)
+# a=[1,2,3,4,5,6,7]
+# a.insert(-2,234)
+# print(a)
+# a.append(45,)
+# print(a)
+# a="0000"
+# b="0001"
+# num=int(input("num: "))
+# str2=str(num)
+# if num>0 :
+#     for i in str2:
+#         if i
+
